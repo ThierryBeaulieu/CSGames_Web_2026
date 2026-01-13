832b21e531d407cebaeecfadedf59bcc3fb16062
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2026-01-13 09:14:38 -0500

    the export ot the server prevented the app from working

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,3 @@ app.use(express.static(PUBLIC_PATH));
 app.use('/api/sprite', spriteRouter.router);
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
-
-module.exports = server;
