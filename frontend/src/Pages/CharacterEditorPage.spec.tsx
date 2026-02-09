3ffc2fb6d957a83628b8a101ca7511eb8db33f27
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2026-02-08 20:41:14 -0500

    removed trees and tests

--- frontend/src/Pages/CharacterEditorPage.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, test } from "vitest";
-import { render } from "@testing-library/react";
-import CharacterEditorPage from "./CharacterEditorPage";
-
-describe("CharacterEditorPage", () => {
-  test("renders without crashing", () => {
-    render(<CharacterEditorPage />);
-  });
-});
--- frontend/src/Pages/EasterEggPage.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, test } from "vitest";
-import { render } from "@testing-library/react";
-import EasterEggPage from "./EasterEggPage";
-
-describe("EasterEggPage", () => {
-  test("renders without crashing", () => {
-    render(<EasterEggPage />);
-  });
-});
